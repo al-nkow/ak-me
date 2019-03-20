14b6213abc7461b1ea1cc3037471e1e535f4d000
Aleksey <al.nkow@gmail.com>
2019-03-20 19:12:56 +0200

    portfolio template

--- README.md
+++ README.md
@@ -8,6 +8,5 @@ Portfolio site
 
 #### TODO:
 1. Add PRELOADER
-4. fix image size inside popup for different screens
 6. sort portfolio - new on top
 9. remove # from url
--- src/modules/portfolio/portfolio.controller.js
+++ src/modules/portfolio/portfolio.controller.js
@@ -1,2 +1,69 @@
-export default class PortfolioController { }
+export default class PortfolioController {
+    constructor() {
+        this.projects = [
+            {
+                sref: 'root.portfolio.sota',
+                imgUrl: '/images/bgsota.jpg',
+                desc: {
+                    logo: '/images/sota-logo.png',
+                    head: 'SOTA',
+                    title: 'Интерактивный контроль строительства',
+                }
+            },
+            {
+                sref: 'root.portfolio.fitness',
+                imgUrl: '/images/bgfitness.jpg',
+            },
+            {
+                sref: 'root.portfolio.utmc',
+                imgUrl: '/images/bgutmc2.jpg',
+                desc: {
+                    head: 'UTMC',
+                    title: 'Unemployment Tax Management Corporation',
+                }
+            },
+            {
+                sref: 'root.portfolio.hrportal',
+                imgUrl: '/images/bghr.jpg',
+            },
+            {
+                sref: 'root.portfolio.gali',
+                imgUrl: '/images/bggali.jpg',
+            },
+            {
+                sref: 'root.portfolio.fundsource',
+                imgUrl: '/images/bgfundsource.jpg',
+                desc: {
+                    head: 'FundSource',
+                    title: 'Crowdfunding platform',
+                }
+            },
+            {
+                sref: 'root.portfolio.zypmedia',
+                imgUrl: '/images/bgzypmedia.jpg',
+                desc: {
+                    head: 'ZypMedia',
+                    title: 'The first local-focused, full-stack, omnichannel platform',
+                }
+            },
+            {
+                sref: 'root.portfolio.potolok',
+                imgUrl: '/images/bgpotolok.jpg',
+            },
+            {
+                sref: 'root.portfolio.prokach',
+                imgUrl: '/images/bgprokach.jpg',
+            },
+            {
+                sref: 'root.portfolio.trafficstars',
+                imgUrl: '/images/bgtrafficstars.jpg',
+            },
+            {
+                sref: 'root.portfolio.runative',
+                imgUrl: '/images/bgrunative.jpg',
+            },
+
+        ];
+    }
+}
 
